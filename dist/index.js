dd9563930f81fa6f4e68925b8d96aea7868d80dc
Aashishh05 <shresthaaashish246@gmail.com>
2026-09-15 21:42:35 +0545

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `hello ${person}, welcome`;
+}
+const username = "Aashish";
+console.log(greet(username));
+//# sourceMappingURL=indes.js.map
