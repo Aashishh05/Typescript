cf7f326021e91b15c51c93d99453b80be9271fe3
Aashishh05 <shresthaaashish246@gmail.com>
2026-09-15 21:56:56 +0545

    read type inferencing and annotation

---
+++ src/typesInTs.ts
@@ -0,0 +1,14 @@
+let drink = "coke"
+
+
+// type inferencing
+let cups = Math.random() > 0.5 ?10:5
+
+let name = "Ram"
+
+// type annotation
+let age:number = 12;
+age = 20;
+
+
+let chaiOrder:boolean
